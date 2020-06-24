3207bcc866c41ef4e5f6cbc6694cca476164d897
ThanhBinhKma <thanhbinhkma27@gmail.com>
2020-06-24 18:22:42 +0700

    gada

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~plugins/axios','~plugins/moment'],
+  plugins: ['~plugins/axios','~plugins/moment', '~plugins/element-ui'],
   /*
    ** Nuxt.js dev-modules
    */
